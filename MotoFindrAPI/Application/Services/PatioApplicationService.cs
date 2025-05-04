d588ff6f3df56873d27fd570448251a53c1990c2
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-04 10:15:21 -0300

    feat: feito o service e a interface do repository.

---
+++ MotoFindrAPI/Application/Services/PatioApplicationService.cs
@@ -0,0 +1,75 @@
+﻿using MotoFindrAPI.Application.Interfaces;
+using MotoFindrAPI.Domain.Entities;
+using MotoFindrAPI.Domain.Interfaces;
+
+namespace MotoFindrAPI.Application.Services
+{
+    public class PatioApplicationService : IPatioApplicationService
+    {
+        private readonly IPatioRepository _repository;
+        public PatioApplicationService(IPatioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> AtualizarPatioAsync(int id, PatioEntity patio)
+        {
+            try
+            {
+                return await _repository.AtualizarAsync(id, patio);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<PatioEntity?> BuscarPatioPorIdAsync(int id)
+        {
+            try
+            {
+                return await _repository.BuscarPorIdAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<PatioEntity>> BuscarTodosPatiosAsync()
+        {
+            try
+            {
+                return await _repository.BuscarTodosAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> DeletarPatioAsync(int id)
+        {
+            try
+            {
+                return await _repository.DeletarAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<PatioEntity> SalvarAsync(PatioEntity patio)
+        {
+            try
+            {
+                return await _repository.SalvarAsync(patio);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ MotoFindrAPI/Domain/Interfaces/IPatioRepository.cs
@@ -0,0 +1,14 @@
+﻿using MotoFindrAPI.Domain.Entities;
+
+namespace MotoFindrAPI.Domain.Interfaces
+{
+    public interface IPatioRepository
+    {
+        Task<IEnumerable<PatioEntity>> BuscarTodosAsync();
+        Task<PatioEntity?> BuscarPorIdAsync(int id);
+        Task<PatioEntity> AdicionarAsync(PatioEntity patio);
+        Task<bool> AtualizarAsync(int id, PatioEntity patio);
+        Task<bool> DeletarAsync(int id);
+        Task<PatioEntity> SalvarAsync(PatioEntity patio);
+    }
+}

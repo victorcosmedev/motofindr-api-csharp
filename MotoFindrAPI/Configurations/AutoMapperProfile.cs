845b2607cb983cc25ff954de894ba003b802cfa5
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-07 20:16:12 -0300

    feat: Inclusão do AutoMapper;

---
+++ MotoFindrAPI/Configurations/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MotoFindrAPI.Application.DTOs;
+using MotoFindrAPI.Domain.Entities;
+namespace MotoFindrAPI.Configurations
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<MotoDTO, MotoEntity>()
+                .ForMember(dest => dest.Motoqueiro, opt => opt.Ignore())
+                .ForMember(dest => dest.Vaga, opt => opt.Ignore())
+                .ReverseMap();
+        }
+    }
+}

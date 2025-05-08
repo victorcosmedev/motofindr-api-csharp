cde2d445196db52d791f6c5279f50fa51b8783cd
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-08 20:20:54 -0300

    feat: Feito o DTO da Vaga.

---
+++ MotoFindrAPI/Application/DTOs/VagaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MotoFindrAPI.Application.DTOs
+{
+    public record VagaDTO
+    {
+        public int IdVaga { get; set; }
+        public int SecaoId { get; set; }
+        public int? MotoId { get; set; }
+        public int NumeroVaga { get; set; }
+        public bool Disponivel { get; set; }
+    }
+}

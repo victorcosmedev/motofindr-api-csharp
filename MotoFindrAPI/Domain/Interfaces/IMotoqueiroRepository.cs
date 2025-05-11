540969b92bdb3ff23bd4bdcb20d1713955a4e627
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-11 10:40:03 -0300

    feat: Feita a implementação do Repositório de motoqueiros

--- MotoFindrAPI/Domain/Interfaces/IMotoqueiroRepository.cs
+++ MotoFindrAPI/Domain/Interfaces/IMotoqueiroRepository.cs
@@ -6,8 +6,8 @@ namespace MotoFindrAPI.Domain.Interfaces
     {
         Task<IEnumerable<MotoqueiroEntity>> BuscarTodosAsync();
         Task<MotoqueiroEntity?> BuscarPorIdAsync(int id);
-        Task<MotoqueiroEntity> SalvarAsync(MotoqueiroEntity moto);
-        Task<bool> AtualizarAsync(int id, MotoqueiroEntity moto);
+        Task<MotoqueiroEntity> SalvarAsync(MotoqueiroEntity entity);
+        Task<bool> AtualizarAsync(int id, MotoqueiroEntity entity);
         Task<bool> DeletarAsync(int id);
     }
 }
--- MotoFindrAPI/Infrastructure/Data/Repositories/MotoqueiroRepository.cs
+++ MotoFindrAPI/Infrastructure/Data/Repositories/MotoqueiroRepository.cs
@@ -1,33 +1,98 @@
-﻿using MotoFindrAPI.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using MotoFindrAPI.Domain.Entities;
 using MotoFindrAPI.Domain.Interfaces;
+using MotoFindrAPI.Infrastructure.Data.AppData;
 
 namespace MotoFindrAPI.Infrastructure.Data.Repositories
 {
     public class MotoqueiroRepository : IMotoqueiroRepository
     {
-        public Task<bool> AtualizarAsync(int id, MotoqueiroEntity moto)
+        private readonly ApplicationContext _context;
+
+        public MotoqueiroRepository(ApplicationContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<MotoqueiroEntity?> BuscarPorIdAsync(int id)
+        public async Task<MotoqueiroEntity> SalvarAsync(MotoqueiroEntity entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Motoqueiro.Add(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<bool> AtualizarAsync(int id, MotoqueiroEntity entity)
+        {
+            try
+            {
+                var motoqueiroExistente = await _context.Motoqueiro.FindAsync(id);
+                if (motoqueiroExistente == null)
+                    throw new Exception($"Não há nenhum motoqueiro com o Id {id}.");
+
+                motoqueiroExistente.NomeUser = entity.NomeUser;
+                motoqueiroExistente.DataAniversarioUser = entity.DataAniversarioUser;
+                motoqueiroExistente.CpfUser = entity.CpfUser;
+                motoqueiroExistente.MotoId = entity.MotoId;
+                motoqueiroExistente.Moto = entity.Moto;
+                motoqueiroExistente.Cnh = entity.Cnh;
+                motoqueiroExistente.Endereco = entity.Endereco;
+
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
-        public Task<IEnumerable<MotoqueiroEntity>> BuscarTodosAsync()
+        public async Task<MotoqueiroEntity?> BuscarPorIdAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var motoqueiro = await _context.Motoqueiro.FindAsync(id);
+                return motoqueiro;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
-        public Task<bool> DeletarAsync(int id)
+        public async Task<IEnumerable<MotoqueiroEntity>> BuscarTodosAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var motoqueiros = await _context.Motoqueiro.ToListAsync();
+                return motoqueiros;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
 
-        public Task<MotoqueiroEntity> SalvarAsync(MotoqueiroEntity moto)
+        public async Task<bool> DeletarAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var motoqueiro = await _context.Motoqueiro.FindAsync(id);
+                if (motoqueiro == null)
+                    throw new Exception($"Motoqueiro de id {id} não existe");
+                _context.Motoqueiro.Remove(motoqueiro);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
